e999e1b2f328ccf71fad6aef229ce45796d6ee83
Max Ageev <ageev.makson@yandex.ru>
2022-07-11 20:39:55 +0300

    int swap to short

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -3,7 +3,7 @@ package lang.print.gaps.task4;
 public class FormulaCreator {
     public static void main(String[] args) {
 
-        int a = 10,b = 10;
+        int a = 0,b = 0;
 
         int result = (9 * (a * a) - 5 * b + 2 + a -7 ) * ((a+b - 4 *a*b)/2);
     }
