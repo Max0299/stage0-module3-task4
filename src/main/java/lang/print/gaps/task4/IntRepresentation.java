93b9719094092a6eb528843ddbbeb98ae8416564
Max Ageev <ageev.makson@yandex.ru>
2022-07-11 20:32:39 +0300

    message

--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,16 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+
+        char c = 99;
+        char a = 97;
+        char h = 104;
+        char r = 114;
+
+        System.out.print(c + "\t");
+        System.out.print(h + "\t");
+        System.out.print(a + "\t");
+        System.out.print(r + "\t");
+
     }
 }
